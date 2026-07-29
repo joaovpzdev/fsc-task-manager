447585d21189e388eccb8a4a752f6a3362ffe97b
joaovpzdev <joaovpz.dev@gmail.com>
2026-07-29 11:19:13 -0300

    feat: add Sidebar and SidebarButtons

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import SidebarButton from "./Sidebarbuttons";
+
+const Sidebar = () => {
+  return (
+    <div className="h-screen w-64 bg-white ">
+      <div className="space-y-4 px-8 py-6">
+        <h1 className="text-xl font-semibold text-[#00ADB5]">Task Manager </h1>
+        <p>
+          {" "}
+          <span className="text-[#00ADB5]">Organizador de Tarefas</span>{" "}
+        </p>
+      </div>
+      <div className="flex flex-col gap-2 p-2">
+        <SidebarButton variant="unselected">Início</SidebarButton>
+        <SidebarButton variant="selected">Minhas tarefas</SidebarButton>
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
