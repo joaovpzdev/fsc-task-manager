d78e1515e026080bf374671b65dabaec2e08c5af
joaovpzdev <joaovpz.dev@gmail.com>
2026-07-29 10:11:24 -0300

    chore:add poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
