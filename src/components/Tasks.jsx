d2eb37131704e07c6408e17491b1de27c1c2c746
joaovpzdev <joaovpz.dev@gmail.com>
2026-07-30 11:20:47 -0300

    feat: add separator between tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,10 @@
 import Button from "./Button"
 import AddIcon from "./assets/fonts/icons/add.svg?react"
 import TrashIcon from "./assets/fonts/icons/trash.svg?react"
+import SunIcon from "./assets/fonts/icons/sun.svg?react"
+import CloudIcon from "./assets/fonts/icons/cloud-sun.svg?react"
+import MoonIcon from "./assets/fonts/icons/moon.svg?react"
+
 
 const Tasks = () => {
     return <div className="w-full px-8 py-16">
@@ -18,6 +22,33 @@ const Tasks = () => {
                             <AddIcon />
                             Adicionar Tarefa
                         </Button>
+                    </div>
+                </div>
+
+                {/*Lista de tarefas */}
+
+                
+                <div className="rounded-xl bg-white p-6">
+                    {/*Tarefas da Manhã */}
+                    <div className="space-y-3">
+                            <div className="flex gap-2 pb-1 border-solid border-b border-[#F4F4F5]">
+                                <SunIcon />
+                                <p className="text-[#9A9C9F] text-sm">Manhã</p>
+                            </div>
+                    </div>
+                    {/*Tarefas da Tarde */}
+                    <div className="my-6 space-y-3">
+                            <div className="flex gap-2 border-b pb-1 border-solid border-[#F4F4F5]">
+                                <CloudIcon />
+                                <p className="text-[#9A9C9F] text-sm">Tarde</p>
+                            </div>
+                    </div>
+                    {/*Tarefas da Noite */}
+                    <div className="space-y-3">
+                            <div className="flex gap-2 pb-1  border-solid border-b border-[#F4F4F5]">
+                                <MoonIcon />
+                                <p className="text-[#9A9C9F] text-sm">Noite</p>
+                            </div>
                     </div>
                 </div>
             </div>
