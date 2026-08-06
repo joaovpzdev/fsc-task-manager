f9929afe05d795bc420ab1aa52b13251e0b7ea35
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-06 16:56:40 -0300

    feat: ensure API is working deleting and add tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -45,15 +45,19 @@ const Tasks = () => {
   };
 
   //Função para deletar uma tarefa
-  const handleTaskDeleteClick = (taskId) => {
+  const handleTaskDeleteClick = async (taskId) => {
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      return toast.error("Erro ao deletar tarefa");
+    }
+
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
     toast.success("Tarefa deletada com sucesso!");
   };
-
-  {
-    /* atualização de progresso das checkboxes */
-  }
 
   //Função para alterar o State
 
